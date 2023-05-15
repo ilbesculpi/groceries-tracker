9a8f2a2a0a0ce5747d37b34272caa9db7bc1891a
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2023-05-15 09:28:46 -0400

    stop tracking Pods

--- Pods/Target Support Files/Pods-GroceriesTracker-GroceriesTrackerUITests/Pods-GroceriesTracker-GroceriesTrackerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GroceriesTracker_GroceriesTrackerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_GroceriesTracker_GroceriesTrackerUITests
-@end
